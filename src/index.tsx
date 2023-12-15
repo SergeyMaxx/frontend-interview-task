156bcff6fe311d399a71d5de9e99a4cfc6e1bb1b
Sergey Maximov <95900933+SergeyMaxx@users.noreply.github.com>
2023-12-15 07:40:42 +0300

    init commit

---
+++ src/index.tsx
@@ -0,0 +1,8 @@
+import {createRoot} from 'react-dom/client'
+import App from './App'
+import reportWebVitals from './reportWebVitals'
+
+createRoot(document.getElementById('root') as HTMLElement)
+  .render(<App/>)
+
+reportWebVitals()
